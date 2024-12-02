bf5f0f9e7d3db480b716a4e0fde7a129e56e58a5
jaky-sheikh <jaky@eu4m.eu>
2024-12-02 14:37:38 +0600

    Navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
-  
+
 
   return (
     <>
-      <h1>Vite + React</h1>
-      
+      <Navbar></Navbar>
+      {/* <h1>Vite + React</h1> */}
+
     </>
   )
 }
